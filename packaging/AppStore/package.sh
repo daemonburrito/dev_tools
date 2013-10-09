02ab205c320b685e6016772d0411ae6f3e2c9f16
Alex Siegman <asiegman@gmail.com>
2013-10-08 17:48:47 -0700

    DOPS-42 tweaks for prod environment in packaging

--- packaging/AppStore/package.sh
+++ packaging/AppStore/package.sh
@@ -5,8 +5,9 @@ set -e
 APPSTORE_REPO_URL=git@github.com:famigo/AppStore.git
 APPSTORE_CO_PATH=AppStore
 BASE_PATH=/mnt/famigo/
-VENV_NAME=venv-appstore
-VENV_PATH=${BASE_PATH}/envs/${VENV_NAME}
+VENV_NAME=AppStore
+VENV_BASE_PATH=envs
+VENV_PATH=${BASE_PATH}/${VENV_BASE_PATH}/${VENV_NAME}
 VERSION_FILE=version
 
 echo "AppStore venv and code packaging script"
@@ -35,7 +36,7 @@ if [ ! -d $VENV_PATH ] ; then
 	virtualenv --distribute --no-site-packages ${VENV_PATH}
 	echo "Since I'm creating a new virtualenv, install django-profiler as it cannot be installed after Django is or it breaks."
 	source ${VENV_PATH}/bin/activate
-	pip install django-profiler==1.1
+	pip -q install django-profiler==1.1
 	deactivate
 fi
 
@@ -43,7 +44,7 @@ source ${VENV_PATH}/bin/activate
 pushd ${APPSTORE_CO_PATH} > /dev/null
 for req in $(find . -name "requirements*.txt") ; do
 	sed -i 's#netaddr==0.7.10#netaddr==0.7.3#g' ${req}
-	pip install -r ${req}
+	pip -q install -r ${req}
 	git checkout ${req}
 done
 popd > /dev/null
@@ -54,18 +55,18 @@ echo -n "Bumping version... "
 
 increment_version ()
 {
-  declare -a part=( ${1//\./ } )
-  declare    new
-  declare -i carry=1
+	declare -a part=( ${1//\./ } )
+	declare    new
+	declare -i carry=1
 
-  for (( CNTR=${#part[@]}-1; CNTR>=0; CNTR-=1 )); do
-    len=${#part[CNTR]}
-    new=$((part[CNTR]+carry))
-    [ ${#new} -gt $len ] && carry=1 || carry=0
-    [ $CNTR -gt 0 ] && part[CNTR]=${new: -len} || part[CNTR]=${new}
-  done
-  new="${part[*]}"
-  echo -e "${new// /.}"
+	for (( CNTR=${#part[@]}-1; CNTR>=0; CNTR-=1 )); do
+		len=${#part[CNTR]}
+		new=$((part[CNTR]+carry))
+		[ ${#new} -gt $len ] && carry=1 || carry=0
+		[ $CNTR -gt 0 ] && part[CNTR]=${new: -len} || part[CNTR]=${new}
+	done
+	new="${part[*]}"
+	echo -e "${new// /.}"
 } 
 
 # Incrementing the version has to be done *before* any of the copies below
@@ -78,9 +79,10 @@ dch -v "$(cat $VERSION_FILE)-1" "New version, Jenkins Build: $BUILD_NUMBER"
 echo "Setting up packaging environment"
 rm -rf $PACKAGE_DIR && mkdir -p $PACKAGE_DIR
 pushd $PACKAGE_DIR > /dev/null
-rsync -avh --delete ../debian .
-rsync -avh --delete --exclude '*.pyc' ${VENV_PATH} .
-rsync -avh --delete --exclude '*.pyc' ../${APPSTORE_CO_PATH} .
-debuild -us -uc
+rsync -ah --delete ../debian .
+mkdir -p ${VENV_BASE_PATH}
+rsync -ah --delete --exclude '*.pyc' ${VENV_PATH} ${VENV_BASE_PATH}/
+rsync -ah --delete --exclude '*.pyc' ../${APPSTORE_CO_PATH} .
+debuild --no-lintian --no-tgz-check -us -uc
 popd > /dev/null
 
